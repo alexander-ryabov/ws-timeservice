845b2dd75ede6d28b12ce3b9a8ea64a42cc58a72
Alexander Ryabov <alexander.ryabov@gmail.com>
2013-08-23 18:54:26 +0400

    TimeService plus client

--- service-java/src/main/java/demo/jaxws/timeservice/client/TimeServiceClient.java
+++ service-java/src/main/java/demo/jaxws/timeservice/client/TimeServiceClient.java
@@ -13,7 +13,7 @@ public class TimeServiceClient {
     public static void main(String[] args) throws MalformedURLException {
         Service service = Service.create(
             new URL("http://127.0.0.1:9000/timeserver"),
-            new QName("http://timeservice.jaxws.demo/","TimeServerImplService") //Qualified name is taken from wsdl: {http://timeservice.jaxws.demo/}TimeServerImplService
+            new QName("http://timeservice.jaxws.demo/","TimeServiceImplService") //Qualified name is taken from wsdl: {http://timeservice.jaxws.demo/}TimeServerImplService
         );
         TimeService timeService = service.getPort(TimeService.class);  // Service endpoint interface
         System.out.println("Time as long:   " + timeService.getTimeAsLong());
